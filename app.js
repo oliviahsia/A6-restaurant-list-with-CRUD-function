853299938c866f2ef1b69f4d7653307bcf5ac0a6
olivia <olivia562100@gmail.com>
2020-12-17 21:25:19 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const db = mongoose.connection
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongoose connection!')
+})
 
 app.get('/', (req, res) => {
   res.send('hello world')
