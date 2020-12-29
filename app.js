4623922078a1dbec31ad43771a9264a7899319aa
olivia <olivia562100@gmail.com>
2020-12-29 11:28:33 +0800

    refactor:modify mongoose config

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 
 app.use(express.static('public'))
-
-
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
 app.use(methodOverride('_method'))
 app.use(routes)
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-
-db.once('open', () => {
-  console.log('mongoose connection!')
-})
 
 app.engine('hbs', exphbs({
   defaultLayout: 'main', extname: '.hbs'
--- models/seeds/restSeeder.js
+++ models/seeds/restSeeder.js
@@ -1,19 +1,10 @@
-const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')
 
 // 載入json種子資料
 const restList = require('./restaurant.json')
-
-const db = mongoose.connection
-mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
-
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  console.log('mongoose connection!')
   for (let i = 0; i < restList.results.length; i++) {
     Restaurant.create(restList.results[i])
   }
